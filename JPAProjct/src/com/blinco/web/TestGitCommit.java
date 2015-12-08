d592afe7346c64d4c317be74dec2e3c2aeafa160
kanhublinco <khu@blinco.com>
2015-12-08 00:00:28 -0500

    test git

---
+++ JPAProjct/src/com/blinco/web/TestGitCommit.java
@@ -0,0 +1,12 @@
+package com.blinco.web;
+
+public class TestGitCommit {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int j = 1;
+		 System.out.println("i + j = " +(i+j));
+
+	}
+
+}
